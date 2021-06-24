8586ed1c9aa93bd8029a242133600a64bc87ce84
yasar <imYasar07@gmail.com>
2021-06-24 12:15:40 +0530

    added about me

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -18,6 +18,14 @@ const Home = () => {
                 <div className="anim"></div>
                 <p className="para">I am <span id="text">yasar arafath</span> a <span id="text">front end</span> developer from kerala</p>
             </section>
+            <section>
+                <div className="aboutme">
+                    <h1>About me</h1>
+                    <p>
+                        i am an computer science undergraduate student from kerala. iam interested in computer science and how computer's actually works since my childhood days.
+                    </p>
+                </div>
+            </section>
         </div>
     )
 }
