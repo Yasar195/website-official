1bd101653d0939d904cc18d8ec4f027e5babdcf4
yasar <imYasar07@gmail.com>
2021-06-27 10:18:10 +0530

    ui changes

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -5,16 +5,21 @@ const Home =()=>{
     return(
         <div>
             <section>
-                <div className="nav">
-                    <h1>My portfolio</h1>
-                    <h2>Blog</h2>
-                    <h2>Contact me</h2>
-                    <h2>My Projects</h2>
-                </div>
                 <div className="welcome">
                     <h1 className="hai">Hai</h1>
                     <h2 className="yasar">i am yasar arafath welcome to my website</h2> 
                 </div>
+            </section>
+            <section>
+                <div className="welcome">
+                    <h1 className="hai">My projects</h1>
+                </div>
+            </section>
+            <section>
+
+            </section>
+            <section>
+
             </section>
         </div>
     )
