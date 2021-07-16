b9f22fe8fa381f4a1f64246ca10cdca002d9cf23
yasar <imYasar07@gmail.com>
2021-07-16 21:14:33 +0530

    changes to the designs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import Home from './component/Home/Home';
-import Skill from './component/Skill/Skill';
-import Contact from './component/Contact/Contact';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 
 const App = () => {
     return(
-        <div>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/skill" exact component={Skill}/>
-                    <Route path="/contact" exact component={Contact}/>
-                </Switch>
-             </Router>
+        <div className="app">
+            <section>
+                <h1 id="textanim">Hello iam Yasar arafath</h1>
+            </section>
         </div>
     )
 }
