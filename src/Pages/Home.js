3bc6e48e6263f810a75c911abe2a11ac30463632
yasar <imYasar07@gmail.com>
2021-08-08 12:19:32 +0530

    ui changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,63 +3,58 @@ import './Home.css';
 import {motion} from 'framer-motion';
 
 const Home = () => {
+
+    const text = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+        }
+    }
+
     return(
         <div className="Home">
-            <motion.h1
-                initial={{
-                    opacity:0,
-                }}
-                animate={{
-                    opacity: 1,
-                }}
-                transition={{
-                    duration: 2
-                }}
-                className="heading"
-            >Hai</motion.h1>
-            <p className="para">I am Yasar arafath a web-developer from kerala</p>
-            <div className="links">
-                <motion.button 
-                whileHover={{
-                    scale: 1.1,
-                }}
-                whileTap={{
-                    scale: 0.9,
-                }}
-                id="button">
-                    Projects
-                </motion.button>
-                <motion.button
-                whileHover={{
-                    scale: 1.1,
-                }}
-                whileTap={{
-                    scale: 0.9,
-                }}
-                id="button">
-                    About me
-                </motion.button>
-                <motion.button
-                whileHover={{
-                    scale: 1.1,
-                }}
-                whileTap={{
-                    scale: 0.9,
-                }}
-                id="button">
-                    Blog
-                </motion.button>
-                <motion.button
-                whileHover={{
-                    scale: 1.1,
-                }}
-                whileTap={{
-                    scale: 0.9,
-                }}
-                id="button">
-                    Contact me
-                </motion.button>
-            </div>
+            <section>
+                <motion.h1 initial="hidden" animate="visible" transition={{duration: 2}} variants={text} id="hai">Hai</motion.h1>
+                <motion.p variants={text} initial="hidden" animate="visible" transition={{duration: 3}} id="para">I am Yasar arafath a web developer from kerala</motion.p>
+                <motion.div drag
+                initial="hidden"
+                animate="visible"
+                variants={text}
+                transition={{duration: 5}}
+                dragConstraints={{left: 10, right: 10, bottom: 10, top: 10}}
+                whileDrag={{scale: 1.1}} id="purple" className="anim">
+                    <h1>Front-end development</h1>
+                </motion.div>
+                <motion.div drag
+                initial="hidden"
+                animate="visible"
+                variants={text}
+                transition={{duration: 5}}
+                dragConstraints={{left: 10, right: 10, bottom: 10, top: 10}}
+                whileDrag={{scale: 1.1}}
+                id="tomato" className="anim">
+                    <h1>back-end development</h1>
+                </motion.div>
+            </section>
+            <section>
+                <h1 id="about">About me</h1>
+                <p id="about_para">I am a 2nd year undergraduate student persuing degree in computer science and engineering. I am really passionate about what i do. Really open to new opertunities.</p>
+                <motion.div
+                 drag
+                 className="about"
+                 dragConstraints={{left: 10, right: 10, bottom: 10, top: 10}}
+                 whileDrag={{scale: 1.1}}
+                 initial="hidden"
+                 animate="visible"
+                 transition={{duration: 5}}
+                 variants={text}
+                 className="anim_about"
+                >
+                    <h1>Computer science and engineering</h1>
+                </motion.div>
+            </section>
         </div>
     )
 }
